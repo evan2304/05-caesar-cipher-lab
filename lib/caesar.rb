3c7a57fa68626fbfaf54bd3fdcb0a6ee5c88f789
Eva Noertoft <evan2304@hstat.org>
2018-12-03 01:11:31 +0000

    update tests

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -1,6 +1,4 @@
-
 def caesar_decode(string, offset)
-    #insert lowercase and uppercase alphabet. to_a makes it an array then join as a string
     lowercase_alphabet= ("a".."z").to_a.join
     uppercase_alphabet= ("A".."Z").to_a.join
     if string == string.downcase
@@ -19,20 +17,9 @@ def caesar_decode(string, offset)
     end 
 end
 
-#puts "Enter a string:"
-#user_string = gets.chomp
-#puts "Enter an offset:"
-#user_offset = gets.chomp.to_i
-
-#caesar_decode(user_string,user_offset)
-
 def caesar_encode(string, offset)
-    #insert lowercase and uppercase alphabet. to_a makes it an array then join as a string
     lowercase_alphabet= ("a".."z").to_a.join
     uppercase_alphabet= ("A".."Z").to_a.join
-    #combine lowercase and uppercase alphabet
-    #all_alphabet = lowercase_alphabet + uppercase_alphabet
-    #creates an array of of the characters in all_alphabet
     if string == string.downcase
         split_alphabet= lowercase_alphabet.chars
         offset_alphabet = split_alphabet.rotate(offset%26).join
@@ -48,10 +35,3 @@ def caesar_encode(string, offset)
         return string.tr(all_alphabet,offset_alphabet).capitalize
     end 
 end
-
-#puts "Enter a string:"
-#user_string = gets.chomp
-#puts "Enter an offset:"
-#user_offset = gets.chomp.to_i
-
-#caesar_encode(user_string,user_offset)
